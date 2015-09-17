08906fd566ffaba89595456231f0491369d561f9
vinterdo <pio4426@gmail.com>
2015-09-17 19:20:35 +0200

    Terminal renderer

--- src/main/java/com/vinterdo/deusexmachina/block/BTerminal.java
+++ src/main/java/com/vinterdo/deusexmachina/block/BTerminal.java
@@ -3,6 +3,7 @@ package com.vinterdo.deusexmachina.block;
 import com.vinterdo.deusexmachina.DeusExMachina;
 import com.vinterdo.deusexmachina.creativetab.CreativeTabDEM;
 import com.vinterdo.deusexmachina.handler.GuiHandler;
+import com.vinterdo.deusexmachina.tileentity.TETerminal;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
@@ -21,7 +22,7 @@ public class BTerminal extends BTileEntityDEM
 	@Override
 	public TileEntity createNewTileEntity(World p_149915_1_, int p_149915_2_)
 	{
-		return null;
+		return new TETerminal();
 	}
 	
 	@Override
@@ -33,5 +34,23 @@ public class BTerminal extends BTileEntityDEM
 			player.openGui(DeusExMachina.instance, GuiHandler.GuiIDs.TERMINAL.ordinal(), world, x, y, z);
 		}
 		return true;
+	}
+	
+	@Override
+	public int getRenderType()
+	{
+		return -1;
+	}
+	
+	@Override
+	public boolean isOpaqueCube()
+	{
+		return false;
+	}
+	
+	@Override
+	public boolean renderAsNormalBlock()
+	{
+		return false;
 	}
 }
