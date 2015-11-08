b186bfdc9d3fc4b47ee681f53d821e435dae1231
BartoszF <felis.bartosz@gmail.com>
2015-11-08 16:24:02 +0100

    Simple raytrace and block breaking.

--- src/main/java/com/vinterdo/deusexmachina/item/ItemRayGun.java
+++ src/main/java/com/vinterdo/deusexmachina/item/ItemRayGun.java
@@ -2,6 +2,13 @@ package com.vinterdo.deusexmachina.item;
 
 import com.vinterdo.deusexmachina.creativetab.CreativeTabDEM;
 
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
 public class ItemRayGun extends ItemDEM {
 
 	public ItemRayGun() 
@@ -9,5 +16,21 @@ public class ItemRayGun extends ItemDEM {
 		super();
 		this.setUnlocalizedName("rayGun").setCreativeTab(CreativeTabDEM.DEM_TAB);
 	}
+	
+	public ItemStack onItemRightClick(ItemStack par1ItemStack, World world, EntityPlayer player)
+    {	
+    	int x = player.rayTrace(100,1.0f).blockX;
+    	int y = player.rayTrace(100,1.0f).blockY;
+    	int z = player.rayTrace(100,1.0f).blockZ;
+    	
+    		
+    	if(world.getBlock(x, y, z) != Blocks.bedrock)
+    	{
+    		world.getBlock(x, y, z).dropBlockAsItem(world, x, y, z, world.getBlockMetadata(x, y, z), 0);
+    		world.setBlockToAir(x, y, z);
+    	}
+	
+    	return par1ItemStack;
+    }
 
 }
