8eecee00808671ca31211d173921451f1fb2f78d
BartoszF <felis.bartosz@gmail.com>
2015-11-11 15:42:22 +0100

    Properly destroying blocks.

--- src/main/java/com/vinterdo/deusexmachina/item/ItemRayGun.java
+++ src/main/java/com/vinterdo/deusexmachina/item/ItemRayGun.java
@@ -1,13 +1,20 @@
 package com.vinterdo.deusexmachina.item;
 
+import java.util.List;
+
 import com.vinterdo.deusexmachina.creativetab.CreativeTabDEM;
 
+import buildcraft.core.lib.utils.BlockUtils;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.BlockEvent;
 
 public class ItemRayGun extends ItemDEM {
 
@@ -17,20 +24,73 @@ public class ItemRayGun extends ItemDEM {
 		this.setUnlocalizedName("rayGun").setCreativeTab(CreativeTabDEM.DEM_TAB);
 	}
 	
+	@Override
 	public ItemStack onItemRightClick(ItemStack par1ItemStack, World world, EntityPlayer player)
     {	
     	int x = player.rayTrace(100,1.0f).blockX;
     	int y = player.rayTrace(100,1.0f).blockY;
     	int z = player.rayTrace(100,1.0f).blockZ;
     	
-    		
-    	if(world.getBlock(x, y, z) != Blocks.bedrock)
+    	if(!world.isRemote)
     	{
-    		world.getBlock(x, y, z).dropBlockAsItem(world, x, y, z, world.getBlockMetadata(x, y, z), 0);
-    		world.setBlockToAir(x, y, z);
+	    	if(world.getBlock(x, y, z) != Blocks.bedrock)
+	    	{
+	    		Block block = world.getBlock(x, y, z);
+	    		int meta = world.getBlockMetadata(x, y, z);
+	    		
+	    		BlockEvent.BreakEvent breakEvent = new BlockEvent.BreakEvent(x, y, z, world, block, meta,
+						player);
+				MinecraftForge.EVENT_BUS.post(breakEvent);
+				
+				if (!breakEvent.isCanceled()) {
+					List<ItemStack> stacks = BlockUtils.getItemStackFromBlock((WorldServer) world, x, y, z);
+	
+					if (stacks != null) {
+						for (ItemStack s : stacks) {
+							if (s != null) {
+								mineStack(s, world, x, y, z);
+							}
+						}
+					}
+	
+					/*world.playAuxSFXAtEntity(
+							null,
+							2001,
+							x, y, z,
+							Block.getIdFromBlock(block)
+									+ (meta << 12));*/
+	
+					world.setBlockToAir(x, y, z);
+				} else {
+					//hasFailed = true;
+				}
+	    		
+	    	}
     	}
 	
     	return par1ItemStack;
     }
+	
+	public void mineStack(ItemStack stack, World world, int x, int y, int z) 
+	{
+		// Lastly, throw the object away
+		if (stack.stackSize > 0) {
+			float f = world.rand.nextFloat() * 0.8F + 0.1F;
+			float f1 = world.rand.nextFloat() * 0.8F + 0.1F;
+			float f2 = world.rand.nextFloat() * 0.8F + 0.1F;
+
+			EntityItem entityitem = new EntityItem(world,x + f, y + f1 + 0.5F, z + f2, stack);
+
+			//entityitem.lifespan = BuildCraftCore.itemLifespan * 20;
+			entityitem.delayBeforeCanPickup = 10;
+
+			float f3 = 0.05F;
+			entityitem.motionX = (float) world.rand.nextGaussian() * f3;
+			entityitem.motionY = (float) world.rand.nextGaussian() * f3 + 1.0F;
+			entityitem.motionZ = (float) world.rand.nextGaussian() * f3;
+			world.spawnEntityInWorld(entityitem);
+		}
+	}
+
 
 }
