35fe957deb9f85d287071f7520250d60a3a3a98b
Badokk <badokk@gmail.com>
2015-12-07 22:55:18 +0100

    Edited ore generation methods to allow more flexibility.
    Created configuration for oregen, all stored in "/com/vinterdo/deusexmachina/handler/ConfigurationHandler.java".
    
    Important note: Balance not included, each ore has the same spawn rate.

--- src/main/java/com/vinterdo/deusexmachina/handler/ConfigurationHandler.java
+++ src/main/java/com/vinterdo/deusexmachina/handler/ConfigurationHandler.java
@@ -33,7 +33,26 @@ public class ConfigurationHandler
 	
 	private static void loadConfiguration()
 	{
-		boolean generateEssence = configuration.getBoolean("generateEssenceOre", "OreGen", true, "");
+		/* Worldgen configuration */
+		boolean generateCopper = configuration.getBoolean("generateCopper", "OreGen" + configuration.CATEGORY_SPLITTER + "Copper", true, "Whether or not is copper ore generated");
+		int generateCopperMinHeight = configuration.getInt("generateCopperHeightMin", "OreGen" + configuration.CATEGORY_SPLITTER + "Copper", 5, 0, 128, "Minimal height at which Copper is generated");
+		int generateCopperMaxHeight = configuration.getInt("generateCopperHeightMax", "OreGen" + configuration.CATEGORY_SPLITTER + "Copper", 64, 0, 128, "Maximal height at which Copper is generated");
+		int generateCopperVeinsPerChunk = configuration.getInt("generateCopperVeinsPerChunk", "OreGen" + configuration.CATEGORY_SPLITTER + "Copper", 6, 0, 128, "Amount of copper veins generated in a single chunk");
+		int generateCopperBlocksPerVein = configuration.getInt("generateCopperBlocksPerVein", "OreGen" + configuration.CATEGORY_SPLITTER + "Copper", 2, 0, 128, "Amount of copper blocks in a single vein (actual amount is this value plus random(1,4))");
+		
+		boolean generateEssence = configuration.getBoolean("generateEssence", "OreGen" + configuration.CATEGORY_SPLITTER + "Essence", true, "Whether or not is essence ore generated");
+		int generateEssenceMinHeight = configuration.getInt("generateEssenceHeightMin", "OreGen" + configuration.CATEGORY_SPLITTER + "Essence", 5, 0, 128, "Minimal height at which essence is generated");
+		int generateEssenceMaxHeight = configuration.getInt("generateEssenceHeightMax", "OreGen" + configuration.CATEGORY_SPLITTER + "Essence", 64, 0, 128, "Maximal height at which essence is generated");
+		int generateEssenceVeinsPerChunk = configuration.getInt("generateEssenceVeinsPerChunk", "OreGen" + configuration.CATEGORY_SPLITTER + "Essence", 6, 0, 128, "Amount of essence veins generated in a single chunk");
+		int generateEssenceBlocksPerVein = configuration.getInt("generateEssenceBlocksPerVein", "OreGen" + configuration.CATEGORY_SPLITTER + "Essence", 2, 0, 128, "Amount of essence blocks in a single vein (actual amount is this value plus random(1,4))");
+		
+		boolean generateTin = configuration.getBoolean("generateTin", "OreGen" + configuration.CATEGORY_SPLITTER + "Tin", true, "Whether or not is tin ore generated");
+		int generateTinMinHeight = configuration.getInt("generateTinHeightMin", "OreGen" + configuration.CATEGORY_SPLITTER + "Tin", 5, 0, 128, "Minimal height at which tin is generated");
+		int generateTinMaxHeight = configuration.getInt("generateTinHeightMax", "OreGen" + configuration.CATEGORY_SPLITTER + "Tin", 64, 0, 128, "Maximal height at which tin is generated");
+		int generateTinVeinsPerChunk = configuration.getInt("generateTinVeinsPerChunk", "OreGen" + configuration.CATEGORY_SPLITTER + "Tin", 6, 0, 128, "Amount of tin veins generated in a single chunk");
+		int generateTinBlocksPerVein = configuration.getInt("generateTinBlocksPerVein", "OreGen" + configuration.CATEGORY_SPLITTER + "Tin", 2, 0, 128, "Amount of tin blocks in a single vein (actual amount is this value plus random(1,4))");
+		
+		
 		
 		if (configuration.hasChanged())
 			configuration.save();
--- src/main/java/com/vinterdo/deusexmachina/worldgen/WorldGeneratorOres.java
+++ src/main/java/com/vinterdo/deusexmachina/worldgen/WorldGeneratorOres.java
@@ -3,6 +3,8 @@ package com.vinterdo.deusexmachina.worldgen;
 import java.util.Random;
 
 import com.vinterdo.deusexmachina.init.ModBlocks;
+import com.vinterdo.deusexmachina.handler.ConfigurationHandler;
+import net.minecraftforge.common.config.Configuration;
 
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
@@ -13,23 +15,42 @@ import cpw.mods.fml.common.IWorldGenerator;
 
 public class WorldGeneratorOres implements IWorldGenerator
 {
+	Configuration config = ConfigurationHandler.configuration;
+	
+	boolean generateCopper = config.get("OreGen" + config.CATEGORY_SPLITTER + "Copper", "generateCopper", true).getBoolean();
+	int copperMinHeight = config.get("OreGen" + config.CATEGORY_SPLITTER + "Copper", "generateCopperHeightMin", 5).getInt();
+	int copperMaxHeight = config.get("OreGen" + config.CATEGORY_SPLITTER + "Copper", "generateCopperHeightMax", 64).getInt();
+	int copperVeinsPerChunk = config.get("OreGen" + config.CATEGORY_SPLITTER + "Copper", "generateCopperVeinsPerChunk", 6).getInt();
+	int copperBlocksPerVein = config.get("OreGen" + config.CATEGORY_SPLITTER + "Copper", "generateCopperBlocksPerVein", 2).getInt();
+	
+	boolean generateEssence = config.get("OreGen" + config.CATEGORY_SPLITTER + "Essence", "generateEssence", true).getBoolean();
+	int essenceMinHeight = config.get("OreGen" + config.CATEGORY_SPLITTER + "Essence", "generateEssenceHeightMin", 5).getInt();
+	int essenceMaxHeight = config.get("OreGen" + config.CATEGORY_SPLITTER + "Essence", "generateEssenceHeightMax", 64).getInt();
+	int essenceVeinsPerChunk = config.get("OreGen" + config.CATEGORY_SPLITTER + "Essence", "generateEssenceVeinsPerChunk", 6).getInt();
+	int essenceBlocksPerVein = config.get("OreGen" + config.CATEGORY_SPLITTER + "Essence", "generateEssenceBlocksPerVein", 2).getInt();
+	
+	boolean generateTin = config.get("OreGen" + config.CATEGORY_SPLITTER + "Tin", "generateTin", true).getBoolean();
+	int tinMinHeight = config.get("OreGen" + config.CATEGORY_SPLITTER + "Tin", "generateTinHeightMin", 5).getInt();
+	int tinMaxHeight = config.get("OreGen" + config.CATEGORY_SPLITTER + "Tin", "generateTinHeightMax", 64).getInt();
+	int tinVeinsPerChunk = config.get("OreGen" + config.CATEGORY_SPLITTER + "Tin", "generateTinVeinsPerChunk", 6).getInt();
+	int tinBlocksPerVein = config.get("OreGen" + config.CATEGORY_SPLITTER + "Tin", "generateTinBlocksPerVein", 2).getInt();
+	
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator,
 			IChunkProvider chunkProvider)
 	{
-		generateOre(random, chunkX, chunkZ, world, ModBlocks.essenceOre);
-		generateOre(random, chunkX, chunkZ, world, ModBlocks.copperOre);
-		generateOre(random, chunkX, chunkZ, world, ModBlocks.tinOre);
+		if (generateEssence) generateOre(random, chunkX, chunkZ, world, ModBlocks.essenceOre, essenceVeinsPerChunk, essenceBlocksPerVein, essenceMinHeight, essenceMaxHeight);
+		if (generateCopper) generateOre(random, chunkX, chunkZ, world, ModBlocks.copperOre, copperVeinsPerChunk, copperBlocksPerVein, copperMinHeight, copperMaxHeight);
+		if (generateTin) generateOre(random, chunkX, chunkZ, world, ModBlocks.tinOre, tinVeinsPerChunk, tinBlocksPerVein, tinMinHeight, tinMaxHeight);
 	}
 	
-	private void generateOre(Random random, int chunkX, int chunkZ, World world, Block block)
+	private void generateOre(Random random, int chunkX, int chunkZ, World world, Block block, int numOfVeins, int numOfOres, int minY, int maxY)
 	{
-		for (int i = 0; i < 6; i++)
+		for (int i = 0; i < numOfVeins; i++)
 		{
 			int firstBlockXCoord = chunkX * 16 + random.nextInt(16);
-			int firstBlockYCoord = random.nextInt(64);
+			int firstBlockYCoord = minY + random.nextInt(maxY-minY);
 			int firstBlockZCoord = chunkZ * 16 + random.nextInt(16);
-			int numOfOres = 2 + random.nextInt(4);
 			
 			boolean generate = (new WorldGenMinable(block, 0, numOfOres, Blocks.stone)).generate(world, random,
 					firstBlockXCoord, firstBlockYCoord, firstBlockZCoord);
